003bbed76a05e0d378b4e65cdc99d3247e503568
Edwin Zacharias <ezacharias@gmail.com>
2010-10-27 10:34:04 -0400

    Rewrote to be fully tail-calling

--- Sort.hs
+++ Sort.hs
@@ -1,14 +1,16 @@
--- A tail-call optimized sort.
--- This appears to be roughly 30% faster than Data.List.sortBy.
+-- This is an attempt at a fully tail-calling version of sortBy.  I still get a
+-- stack space overflow on large lists (> 1200000 elements) and I don't know
+-- how to get rid of it.
 
 sortBy :: (a -> a -> Ordering) -> [a] -> [a]
-sortBy cmp xs = merge xs []
+sortBy cmp xs = merge [] xs []
   where
-    -- merge :: [a] -> [a] -> [a]
-    merge [] ts = ts
-    merge (x:xs) ts = split x xs [] [] ts
-    -- split :: a -> [a] -> [a] -> [a] -> [a] -> [a]
-    split a [] ys zs ts = merge ys $ a : merge zs ts
-    split a (x:xs) ys zs ts
-      | cmp x a == LT = split a xs (x:ys) zs ts
-      | otherwise = split a xs ys (x:zs) ts
+    -- merge :: [(a, [a])] -> [a] -> [a] -> [a]
+    merge [] [] ts = ts
+    merge ((a,s):ss) [] ts = merge ss s $! (a:ts)
+    merge ss (x:xs) ts = split ss x xs [] [] ts
+    -- split :: [(a, [a])] -> a -> [a] -> [a] -> [a] -> [a] -> [a]
+    split ss a [] ys zs ts = (merge $! (((:) $! (a,ys)) ss)) zs ts
+    split ss a (x:xs) ys zs ts
+      | cmp x a == LT = (split ss a xs $! (x:ys)) zs ts
+      | otherwise = (split ss a xs ys $! (x:zs)) ts
